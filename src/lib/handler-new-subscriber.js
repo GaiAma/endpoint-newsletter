8c3abe32e541750b4f373f99bbc46c6c7cfef3a9
CanRau <cansrau@gmail.com>
2018-06-13 04:01:10 -0500

    FIX: switch to mjml, bump version

--- src/lib/handler-new-subscriber.js
+++ src/lib/handler-new-subscriber.js
@@ -1,5 +1,6 @@
 import { send, json } from 'micro'
-import heml from 'heml'
+// import heml from 'heml'
+import mjml2html from 'mjml'
 import { isEmail } from 'validator'
 import urlEncodedParse from 'urlencoded-body-parser'
 // import isDisposableEmail from 'is-disposable-email'
@@ -47,7 +48,7 @@ export default ({ spark, listPrefix, i18n }) => async (req, res) => {
         i18n,
         link: confirmationLink,
       }),
-      messageHtml: await getHtmlEmail({
+      messageHtml: getHtmlEmail({
         i18n,
         link: confirmationLink,
       }),
@@ -74,77 +75,130 @@ function getTextEmail({ i18n, link }) {
   `
 }
 
-async function getHtmlEmail({ i18n, link }) {
-  try {
-    const { html, errors } = await heml(`
-    <heml>
-      <head>
-        <subject>${i18n.t`subject`}</subject>
-        <style>
-          h1 { font-size: 20px; color: gray; }
-          h2 { margin: 30px 0; }
-          .cta, .orCopy {margin-bottom: 30px; }
-          .ctaButton { background-color: #287482; margin: 0; }
-        </style>
-      </head>
-      <body>
-        <container itemscope itemtype="http://schema.org/EmailMessage">
-          <h1>GaiAma.org Newsletter</h1>
+function getHtmlEmail({ i18n, link }) {
+  const { html, errors } = mjml2html(
+    `
+    <mjml>
+      <mj-head>
+        <mj-attributes>
+          <mj-text font-size="20px" font-family="helvetica" />
+        </mj-attributes>
+      </mj-head>
+      <mj-body>
+        <mj-section full-width="full-width">
+          <mj-column>
+            <mj-text align="center">GaiAma.org Newsletter</mj-text>
 
-          <h2>${i18n.t`title`}</h2>
+            <mj-text>${i18n.t`title`}</mj-text>
 
-          <block class="cta" itemprop="potentialAction" itemscope itemtype="http://schema.org/ConfirmAction">
-            <meta itemprop="name" content="${i18n.t`ctaLabel`}"/>
-            <button class="ctaButton" href="${link}" itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler">
-              <link itemprop="url" href="${link}"/>
-              ${i18n.t`ctaLabel`}
-            </button>
-          </block>
+            <mj-button font-size="16px" font-family="helvetica">${i18n.t`ctaLabel`}</mj-button>
+          </mj-column>
+        </mj-section>
 
-          <block class="orCopy">
-            ${i18n.t`orCopy`}<br>
-            ${link}
-          </block>
+        <mj-section>
+          <mj-column>
+            <mj-text>${i18n.t`orCopy`}</mj-text>
+            <mj-text>${link}</mj-text>
+          </mj-column>
+        </mj-section>
 
-          <block>
-            ${i18n.t`note`}
-          </block>
-        </container>
-        <meta itemprop="description" content="${i18n.t`subject`}"/>
-      </body>
-    </heml>
-  `)
+        <mj-section>
+          <mj-column>
+            <mj-text font-size="17px">
+              ${i18n.t`note`}
+            </mj-text>
+          </mj-column>
+        </mj-section>
 
-    if (errors.length) {
-      throw new Error(JSON.stringify(errors.join(`\n\n`)))
-    }
+      </mj-body>
+    </mjml>
+  `,
+    { minify: true }
+  )
+  // <mj-raw>
+  //   <script type="application/ld+json">
+  //     {
+  //       "@context": "http://schema.org",
+  //       "@type": "EmailMessage",
+  //       "potentialAction": {
+  //         "@type": "ConfirmAction",
+  //         "name": "${i18n.t`ctaLabel`}",
+  //         "handler": {
+  //           "@type": "HttpActionHandler",
+  //           "url": "${link}"
+  //         }
+  //       },
+  //       "description": "${i18n.t`subject`}"
+  //     }
+  //   </script>
+  // </mj-raw>
 
-    return html
-  } catch (error) {
-    return error
+  if (errors.length) {
+    throw new Error(JSON.stringify(errors, null, 2))
   }
-}
 
-// const liste = require(`./subscribers.json`)
-// await spark.recipientLists.delete(listId)
-// const created = await spark.recipientLists.create({
-//   id: listId,
-//   name: `GaiAma Newsletter EN`,
-//   description: ``,
-//   total_accepted_recipients: 2,
-//   recipients: liste
-//     .filter(x => x.approved && x.language === `en`)
-//     .map(x => ({
-//       address: {
-//         email: x.email,
-//         name: x.name,
-//       },
-//       metadata: {
-//         lang: x.language,
-//         id: cuid(),
-//         date: x.date,
-//         isConfirmed: false,
-//       },
-//       return_path: `newsletter@mail.gaiama.org`,
-//     })),
-// })
+  return html
+  // try {
+  //   const { html, errors } = await heml(`
+  //   <heml>
+  //     <head>
+  //       <subject>${i18n.t`subject`}</subject>
+  //       <style>
+  //         h1 { font-size: 20px; color: gray; }
+  //         h2 { margin: 30px 0; }
+  //         .cta, .orCopy {margin-bottom: 30px; }
+  //         .ctaButton { background-color: #287482; margin: 0; }
+  //       </style>
+  //       <script type="application/ld+json">
+  //         {
+  //           "@context": "http://schema.org",
+  //           "@type": "EmailMessage",
+  //           "potentialAction": {
+  //             "@type": "ConfirmAction",
+  //             "name": "${i18n.t`ctaLabel`}",
+  //             "handler": {
+  //               "@type": "HttpActionHandler",
+  //               "url": "${link}"
+  //             }
+  //           },
+  //           "description": "Approval request for John's $10.13 expense for office supplies"
+  //         }
+  //         </script>
+  //     </head>
+  //     <body>
+  //       <container itemscope itemtype="http://schema.org/EmailMessage">
+  //         <h1>GaiAma.org Newsletter</h1>
+
+  //         <h2>${i18n.t`title`}</h2>
+
+  //         <block class="cta" itemprop="potentialAction" itemscope itemtype="http://schema.org/ConfirmAction">
+  //           <meta itemprop="name" content="${i18n.t`ctaLabel`}"/>
+  //           <button class="ctaButton" href="${link}" itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler">
+  //             <link itemprop="url" href="${link}"/>
+  //             ${i18n.t`ctaLabel`}
+  //           </button>
+  //         </block>
+
+  //         <block class="orCopy">
+  //           ${i18n.t`orCopy`}<br>
+  //           ${link}
+  //         </block>
+
+  //         <block>
+  //           ${i18n.t`note`}
+  //         </block>
+  //       </container>
+  //       <meta itemprop="description" content="${i18n.t`subject`}"/>
+  //     </body>
+  //   </heml>
+  // `)
+
+  //   if (errors.length) {
+  //     throw new Error(JSON.stringify(errors.join(`\n\n`)))
+  //   }
+
+  //   return html
+  // } catch (error) {
+  //   return error
+  // }
+}
