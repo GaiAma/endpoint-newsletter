f7991bbdfab34d75c0702dfe47020674519e575e
CanRau <cansrau@gmail.com>
2018-06-13 05:49:29 -0500

    add getList shorthand api

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,8 @@
-export { default as addSubscriber } from './add-subscriber'
+export { default as addSubscriber } from './add-update-subscriber'
 export { default as sendVerification } from './send-verification'
 export { default as isExistingUser } from './is-existing-user'
+
+export const getList = ({ spark, listId }) =>
+  spark.recipientLists.get(listId, {
+    show_recipients: true,
+  })
